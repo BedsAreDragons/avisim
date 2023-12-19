7b5051d3f6a6b9f76a206985e26d888331ebb160
BedsAreDragons <85127998+BedsAreDragons@users.noreply.github.com>
2023-12-19 20:07:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+// your code
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
 
